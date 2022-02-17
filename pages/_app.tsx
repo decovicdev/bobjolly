d0437f7b16b081ea34ec919486bfa5cbca4bbe3a
Oren <oren.menache@gmail.com>
2022-02-17 14:30:37 +0200

    Another go at Pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,9 @@ import Layout from "../components/shared/Layout";
 import theme from "../config/theme";
 import stripe from "../config/stripe";
 import Loading from "../components/shared/Loading";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as fbq from "../utils/pixel";
 
 //https://youtu.be/QAdtc7VWuNE?t=162 GOOGLE ANALYTICS INSTALLATION
 
@@ -20,8 +23,42 @@ function MyApp({ Component, pageProps }: AppProps) {
     setIsLoading(false);
   }, []);
 
+  const router = useRouter();
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Bobjolly</title>
