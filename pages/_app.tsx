45751488c2d704945ff68061380a808de841ed21
Oren <oren.menache@gmail.com>
2022-02-17 14:23:19 +0200

    undone fbPixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,29 +10,11 @@ import Layout from "../components/shared/Layout";
 import theme from "../config/theme";
 import stripe from "../config/stripe";
 import Loading from "../components/shared/Loading";
-import Script from "next/script";
-import router from "next/router";
 
 //https://youtu.be/QAdtc7VWuNE?t=162 GOOGLE ANALYTICS INSTALLATION
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("143022007863352", undefined, {
-          autoConfig: true,
-          debug: true,
-        }); // facebookPixelId
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  }, [router.events]);
 
   useEffect(() => {
     setIsLoading(false);
