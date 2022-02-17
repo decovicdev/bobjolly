20c6e9ebcf90661140147bdbe0a7674a454eb314
DecovicHunter <ahmed_abdikani@hotmail.com>
2022-02-18 02:52:46 +0800

    loading animation update

--- components/shared/Loading.tsx
+++ components/shared/Loading.tsx
@@ -1,7 +1,30 @@
-import { VStack } from '@chakra-ui/react';
+import { HStack, VStack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
 import Logo from './Logo';
+
+const transition = {
+  duration: 0.8,
+  ease: 'linear',
+  repeat: Infinity,
+  repeatType: 'loop',
+} as const;
+const initial = {
+  opacity: 0.3,
+  scale: 1,
+};
+const animate = {
+  opacity: 0,
+  scale: 2,
+};
+
+const style = {
+  borderRadius: '50%',
+  aspectRatio: '1',
+  background: 'purple',
+  width: 200,
+  position: 'absolute',
+} as const;
 
 interface LoadingProps {}
 
@@ -9,20 +32,21 @@ const Loading: React.FC<LoadingProps> = (props) => {
   return (
     <VStack h='100vh' justify='center'>
       <motion.div
-        initial={{ opacity: 1, scale: 1 }}
-        animate={{
-          opacity: 0.2,
-          scale: 1.5,
-        }}
-        transition={{
-          duration: 1,
-          ease: 'easeInOut',
-          repeat: Infinity,
-          repeatType: 'reverse',
-        }}
+        initial={initial}
+        animate={animate}
+        transition={transition}
+        style={style}
+      />
+      <HStack
+        zIndex={1}
+        boxSize={240}
+        justify='center'
+        borderRadius='50%'
+        bg='white'
+        boxShadow='lg'
       >
         <Logo />
-      </motion.div>
+      </HStack>
     </VStack>
   );
 };
